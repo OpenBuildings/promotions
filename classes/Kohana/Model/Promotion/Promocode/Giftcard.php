2cfd866aeb6704f08117daf99ca348f0a2b29cd2
Ivan Kerin <ikerin@gmail.com>
2014-02-26 11:21:57 +0200

    fix giftcard promo
    apply the amount only once per purchase, not once per store purchase.
    The amount is split between store purchases

--- classes/Kohana/Model/Promotion/Promocode/Giftcard.php
+++ classes/Kohana/Model/Promotion/Promocode/Giftcard.php
@@ -36,8 +36,10 @@ class Kohana_Model_Promotion_Promocode_Giftcard extends Model_Promotion_Promocod
 
 	public function price_for_purchase_item(Model_Purchase_Item $purchase_item)
 	{
+		$store_purchases_count = $purchase_item->store_purchase->purchase->store_purchases->count();
+
 		return $this->amount
 			->monetary($purchase_item->monetary())
-			->multiply_by(-1);
+			->multiply_by(-1 / $store_purchases_count);
 	}
 }
