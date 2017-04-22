532e752cdca5f4ba4a98ddf0afeec1e2f4fdfa4a
Haralan Dobrev <hkdobrev@gmail.com>
2017-04-22 14:46:51 +0300

    Use Composer with a classmap autoloader to tests/classes

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,15 +1,5 @@
 <?php
 
-spl_autoload_register(function($class)
-{
-	$file = __DIR__.DIRECTORY_SEPARATOR.'classes'.DIRECTORY_SEPARATOR.str_replace('_', '/', $class).'.php';
-
-	if (is_file($file))
-	{
-		require_once $file;
-	}
-});
-
 require_once __DIR__.'/../vendor/autoload.php';
 
 Kohana::modules(array(
